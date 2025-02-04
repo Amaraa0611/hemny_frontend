d1648f442ff4b39fa65459708edd1370f56908e3
Amartuvshin Altangerel <amartuvshinaltangerel@Amartuvshins-MacBook-Pro.local>
2025-02-04 19:53:21 +0800

    changed con string

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CashbackMerchant } from '../types/cashback';
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5001/api';
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
