fc6eceee7867e4b88029a56dbd27b9ca59eb239a
Amartuvshin Altangerel <amartuvshineducation@gmail.com>
2025-06-25 10:03:58 +0900

    update pg

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,13 +17,13 @@ const Home = () => {
         <meta property="og:url" content="https://hemny.mn/" />
         <meta property="og:title" content="Hemny.mn – Хэмнэлт, урамшуулал, хөнгөлөлтийн төвлөрсөн платформ" />
         <meta property="og:description" content="Монголын хамгийн том хэмнэлт, урамшуулал, хөнгөлөлтийн платформ – Hemny.mn дээр хамгийн шилдэг саналуудыг олж аваарай!" />
-        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:image" content="https://hemny.mn/og-image.jpg" />
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content="https://hemny.mn/" />
         <meta name="twitter:title" content="Hemny.mn – Хэмнэлт, урамшуулал, хөнгөлөлтийн төвлөрсөн платформ" />
         <meta name="twitter:description" content="Монголын хамгийн том хэмнэлт, урамшуулал, хөнгөлөлтийн платформ – Hemny.mn дээр хамгийн шилдэг саналуудыг олж аваарай!" />
-        <meta name="twitter:image" content="/og-image.jpg" />
+        <meta name="twitter:image" content="https://hemny.mn/og-image.jpg" />
       </Head>
       <main>
         <Hero />
